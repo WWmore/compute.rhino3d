8629e9959209b19b84418b4902caef32a8c56c96
WWmore <i1103552872@gmail.com>
2022-01-05 23:14:22 +0300

    update ex

--- src/ghhops-server-py/examples/hops_hui.py
+++ src/ghhops-server-py/examples/hops_hui.py
@@ -1,6 +1,7 @@
 """Hops flask middleware example"""
 from flask import Flask
 import ghhops_server as hs
+from numpy.lib.polynomial import poly1d
 import rhino3dm
 import numpy as np
 #-------------------------------------------------------
@@ -9,7 +10,7 @@ import sys
 path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 path = path+'\ArchitecturalGeometry'
 sys.path.append(path)###==C:\Users\WANGH0M\Documents\GitHub\ArchitecturalGeometry   
-#print(path)
+
 from geometrylab.optimization.gridshell import Gridshell
 from huilab.huigridshell.gridshell_agnet import Gridshell_AGNet
 from HOPS.hops_agnet import AGNet
@@ -27,7 +28,7 @@ hops: hs.HopsFlask = hs.Hops(app)
 #                      OPTIMIZATIONS
 #
 #==================================================================
-def _reading_mesh(mesh,opt=False):
+def _reading_mesh(mesh,opt=False,**kwargs):
     ### GET THE VERTEXLIST+FACELIST OF INPUT MESH:
     vlist,flist = [],[]
     for i in range(len(mesh.Vertices)):
@@ -44,30 +45,30 @@ def _reading_mesh(mesh,opt=False):
         M = Gridshell()
     else:
         ### meshpy.py-->quadrings.py-->gridshell_agnet.py
-        M = Gridshell_AGNet()
+        M = Gridshell_AGNet(**kwargs)
     M.make_mesh(varr, farr) ###Refer gui_basic.py/open_obj_file()
     return M
 
 ### MAIN OPTIMIZATION:
 @hops.component(
-    "/read_1mesh",
+    "/read_1_mesh",
     name="processMesh",
     nickname="pM",
     description="Process a mesh.",
     inputs=[
-        hs.HopsMesh("mesh", "m", "The mesh to process."),
-        hs.HopsInteger("iter","n","num of iteration."),
-        hs.HopsNumber("fairness","w1","weight of fairness.",default=0.005),
-        hs.HopsNumber("closness","w2","weight of self-closness.",default=0.01),
-        hs.HopsNumber("direction","d","direction of polyline.",default=0),
+        hs.HopsMesh("mesh", "Mesh", "The mesh to process."),
+        hs.HopsInteger("iteration","iter","num of iteration."),
+        hs.HopsNumber("fairness","w1(fair)","weight of fairness.",default=0.005),
+        hs.HopsNumber("closness","w2(closeness)","weight of self-closness.",default=0.01),
+        hs.HopsNumber("direction","direction","direction of polyline.",default=0),
         hs.HopsString("web","web","constraint of net or web"),
         hs.HopsBoolean("Restart","Restart","Restart the optimization",default=False),
     ],
-    outputs=[hs.HopsMesh("mesh", "m", "The new mesh."),
-            hs.HopsPoint("an","V","all vertices"),
-            hs.HopsPoint("vn","N","normals at V")]
+    outputs=[hs.HopsMesh("mesh", "mesh", "The new mesh."),
+            hs.HopsPoint("an","Vertices","all vertices"),
+            hs.HopsPoint("vn","Normal","normals at V")]
 )
-def read_1mesh(mesh:rhino3dm.Mesh,n,w1,w2,d,web,restart=False):
+def main(mesh:rhino3dm.Mesh,n,w1,w2,d,web,restart=False):
     #assert isinstance(mesh, rhino3dm.Mesh)
     #m=rhino3dm.Mesh()
     ###-------------------------------------------
@@ -113,17 +114,17 @@ def read_1mesh(mesh:rhino3dm.Mesh,n,w1,w2,d,web,restart=False):
 #==================================================================
 ### PLOTING CHECKER-VERTICES:
 @hops.component(
-    "/checker_tian_vertex",
+    "/checker_tianvertex",
     name="checkerVertex",
     nickname="ckv",
     description="Get checker vertices.",
     inputs=[
-        hs.HopsMesh("mesh", "m", "The optimized mesh.")],
+        hs.HopsMesh("mesh", "Mesh", "The optimized mesh.")],
     outputs=[hs.HopsPoint("P1","P1","checker vertices P1"),
             hs.HopsPoint("P2","P2","checker vertices P2"),
             ]
 )
-def main(mesh:rhino3dm.Mesh):
+def checker_tian_vertex(mesh:rhino3dm.Mesh):
     M = _reading_mesh(mesh)
     Vr,Vb = M.plot_checker_group_tian_select_vertices()
     Pr = [rhino3dm.Point3d(r[0],r[1],r[2]) for r in Vr]
@@ -131,49 +132,90 @@ def main(mesh:rhino3dm.Mesh):
     return Pr,Pb
 #==================================================================
 
-# @hops.component(
-#     "/polyline",
-#     name="checkerVertex",
-#     nickname="ckv",
-#     description="Get checker vertices.",
-#     inputs=[
-#         hs.HopsMesh("mesh", "m", "The optimized mesh."),
-#         hs.HopsNumber("direction","d","direction of polyline.",default=0),
-#         hs.HopsString("web","web","constraint of net or web"),
-#     ],
-#     outputs=[hs.HopsPoint("P1","P1","checker vertices P1"),
-#             hs.HopsPoint("P2","P2","checker vertices P2"),
-#             ]
-# )
-# def polyline(mesh:rhino3dm.Mesh,d,web):
-#     ###-------------------------------------------
-#     ### GET THE VERTEXLIST+FACELIST OF INPUT MESH:
-#     vlist,flist = [],[]
-#     for i in range(len(mesh.Vertices)):
-#         vlist.append([mesh.Vertices[i].X,mesh.Vertices[i].Y,mesh.Vertices[i].Z])
+@hops.component(
+    "/polyline_4_directions",
+    name="polylines",
+    nickname="pl",
+    description="Get 4 familes of polylines.",
+    inputs=[
+        hs.HopsMesh("mesh", "Mesh", "The optimized mesh.")],
+    outputs=[
+            hs.HopsCurve("polyline1", "pl1", access=hs.HopsParamAccess.LIST),
+            hs.HopsCurve("polyline2", "pl2", access=hs.HopsParamAccess.LIST),
+            hs.HopsCurve("polyline3", "pl3", access=hs.HopsParamAccess.LIST),
+            hs.HopsCurve("polyline4", "pl4", access=hs.HopsParamAccess.LIST),
+            ]
+)
+def polyline(mesh:rhino3dm.Mesh):
+    M = _reading_mesh(mesh)
+    V = M.vertexlist
+    ipl11,ipl12,ipl21,ipl22,ipl31,ipl32,ipl41,ipl42 = M.plot_4family_polylines()
+    pl1,pl2,pl3,pl4 = [],[],[],[]
+    for i in range(len(ipl11)):
+        a = rhino3dm.Point3d(V[ipl11][i][0],V[ipl11][i][1],V[ipl11][i][2])
+        b = rhino3dm.Point3d(V[ipl12][i][0],V[ipl12][i][1],V[ipl12][i][2])
+        pl1.append(rhino3dm.LineCurve(a,b))
+    for i in range(len(ipl21)):
+        a = rhino3dm.Point3d(V[ipl21][i][0],V[ipl21][i][1],V[ipl21][i][2])
+        b = rhino3dm.Point3d(V[ipl22][i][0],V[ipl22][i][1],V[ipl22][i][2])
+        pl2.append(rhino3dm.LineCurve(a,b))
+    for i in range(len(ipl31)):
+        a = rhino3dm.Point3d(V[ipl31][i][0],V[ipl31][i][1],V[ipl31][i][2])
+        b = rhino3dm.Point3d(V[ipl32][i][0],V[ipl32][i][1],V[ipl32][i][2])
+        pl3.append(rhino3dm.LineCurve(a,b))
+    for i in range(len(ipl41)):
+        a = rhino3dm.Point3d(V[ipl41][i][0],V[ipl41][i][1],V[ipl41][i][2])
+        b = rhino3dm.Point3d(V[ipl42][i][0],V[ipl42][i][1],V[ipl42][i][2])
+        pl4.append(rhino3dm.LineCurve(a,b))
 
-#     for i in range(mesh.Faces.Count):
-#         flist.append(list(mesh.Faces[i]))
-
-#     varr = np.array(vlist)
-#     farr = np.array(flist)
-
-#     M = Gridshell_AGNet()
-#     M.make_mesh(varr, farr) ###Refer gui_basic.py/open_obj_file()
-
-#     constraints = {
-#         'direction_poly' : d,
-#         web : True,
-#     }
-
-#     AG = AGNet(**constraints)
-#     AG.mesh = M
-#     Pb,Py = AG.plot_checker_group_tian_select_vertices()
-#     return Pb,Py
-
+    return pl1,pl2,pl3,pl4
+#==================================================================
 
 
-
+@hops.component(
+    "/beziersss",
+    name="bezier",
+    nickname="bz",
+    description="Get Bezier splines.",
+    inputs=[
+        hs.HopsMesh("mesh", "Mesh", "The optimized mesh."),
+        hs.HopsString("web","web","constraint of net or web"),
+        hs.HopsVector("VN","VN","vertex normals",access=hs.HopsParamAccess.LIST),
+        hs.HopsInteger("i-th","ipoly","which polyline"), ## only 1,2,3,4
+        hs.HopsNumber("weight","weight","which polyline",default=0.005),
+        hs.HopsBoolean("checker","ck","switch if at checker-vertices",default=False),
+        ],
+    outputs=[
+            hs.HopsPoint("ctrlP","P","all ctrl points"),
+            hs.HopsPoint("an","V","anchor points"),
+            hs.HopsPoint("e1","e1","unit tangent vector"),
+            hs.HopsPoint("e2","e2","unit principal normal vector"),
+            hs.HopsPoint("e3","e3","unit binormal vector"),
+            hs.HopsPoint("r","r","unit ruling vector"),
+            ]
+)
+def bezier(mesh:rhino3dm.Mesh,web,vn:rhino3dm.Vector3d,i,w,is_ck):
+    x = [n.X for n in vn]
+    y = [n.Y for n in vn]
+    z = [n.Z for n in vn]
+    VN = np.c_[x,y,z]
+    setting = {
+        web : True,
+        'VN' : VN,
+        'set_Poly' : i,
+        'weight_CtrlP' : w,
+        'is_Checker' : is_ck,
+    }
+    M = _reading_mesh(mesh,**setting)
+    p,an,e1,e2,e3,r = M.set_quintic_bezier_splines()
+    P = [rhino3dm.Point3d(a[0],a[1],a[2]) for a in p]
+    AN = [rhino3dm.Point3d(a[0],a[1],a[2]) for a in an]
+    E1 = [rhino3dm.Point3d(a[0],a[1],a[2]) for a in e1]
+    E2 = [rhino3dm.Point3d(a[0],a[1],a[2]) for a in e2]
+    E3 = [rhino3dm.Point3d(a[0],a[1],a[2]) for a in e3]
+    R = [rhino3dm.Point3d(a[0],a[1],a[2]) for a in r]
+    return P,AN,E1,E2,E3,R
+#==================================================================
 
 if __name__ == "__main__":
     #app.run(host="10.8.0.10",debug=True)
--- src/ghhops-server-py/examples/hops_hui_beiyong.py
+++ src/ghhops-server-py/examples/hops_hui_beiyong.py
@@ -72,6 +72,15 @@ def arrayss(n):
     return x
 #==================================================================
 
+# pt1,pt2,pt3,pt4 = [],[],[],[]
+    # for pl in ipl1:
+    #     pt1.append([rhino3dm.Point3d(V[ip][0],V[ip][1],V[ip][2]) for ip in pl])
+    # for pl in ipl2:
+    #     pt2.append([rhino3dm.Point3d(V[ip][0],V[ip][1],V[ip][2]) for ip in pl])
+    # for pl in ipl3:
+    #     pt3.append([rhino3dm.Point3d(V[ip][0],V[ip][1],V[ip][2]) for ip in pl])
+    # for pl in ipl4:
+    #     pt4.append([rhino3dm.Point3d(V[ip][0],V[ip][1],V[ip][2]) for ip in pl])
 # @hops.component(
 #     "/input_mesh",
 #     name="processMesh",
@@ -234,3 +243,177 @@ def arrayss(n):
 #         normal.Add(n[0],n[1],n[2])
 
 #     return Mout,anchor,normal
+
+
+
+
+    def get_poly_quintic_Bezier_spline_crvs_checker(self,mesh,normal,
+                                                    efair=0.01,
+                                                    is_asym_or_geo=True,
+                                                    diagpoly=False,
+                                                    is_one_or_another=False,
+                                                    is_checker=1,
+                                                    is_Frame=False,
+                                                    is_onlyinner=False,
+                                                    is_ruling=False,
+                                                    is_dense=False,num_divide=5,
+                                                    is_modify=False,
+                                                    is_smooth=0.0):
+        "For each polyline, 1.[Pi,Ti,Ni,ki] 2.opt to get ctrl-p,polygon,crv"
+        from huilab.huimesh.curvature import frenet_frame
+        from huilab.huimesh.bezierC2Continuity import BezierSpline
+        V = mesh.vertices
+        N = normal ### surf_normal n
+        iall = self.get_both_isopolyline(diagpoly,is_one_or_another,only_inner=False)
+        an = np.array([0,0,0])
+        ruling = np.array([0,0,0])
+        all_kg=all_kn=all_k=all_tau=np.array([])
+        arr = np.array([],dtype=int)
+        varr = np.array([],dtype=int)
+        num = 0
+        #if is_Frame:
+        P=Pl=Pr = np.array([0,0,0])
+        crvPl=crvPr = np.array([0,0,0])
+        frm1=frm2=frm3 = np.array([0,0,0])
+ 
+        seg_q1234,seg_vl, seg_vr = np.array([0,0,0]),[],[]
+        ctrlP = []
+        kck = is_checker
+        
+        if kck !=1:
+            if self._rot_patch_matrix is not None:
+                num_poly = 1
+                if diagpoly:
+                    num_poly -= 1 ## -=2 IF PATCH, ELIF ROTATIOANL =-1
+            elif self.patch_matrix is not None:
+                num_poly,num_allpoly = 0,len(iall)-1
+                if diagpoly:
+                    ##NOTE: TOSELECT:
+                    num_poly -= 2 ## -=2 IF PATCH, ELIF ROTATIOANL =-1
+                    num_allpoly += 2
+            
+        row_list = []
+        for iv in iall:
+            "Except two endpoints on the boudnary"
+            bool_value=False
+            if kck !=1:
+                ###"if only-patch-shape:"
+                bool_value = kck !=0 and 0<num_poly and num_poly<num_allpoly and num_poly%(kck)==0 and len(iv)>=(kck*3+1)
+                ###"if rotational-shape"#TODO
+                #bool_value = kck !=0 and num_poly%(kck)==0 and len(iv)>=(kck*3+1):
+            else:
+                bool_value = len(iv)>=4
+
+            if bool_value:
+                iv_sub,iv_sub1,iv_sub3 = iv[kck:-kck],iv[kck-1:-kck-1],iv[kck+1:-kck+1]
+                Pi = V[iv_sub] # n-8
+                frame = frenet_frame(Pi,V[iv_sub1],V[iv_sub3])
+                "using surface normal computed by A-net or G-net"
+                Ti,Ef2,Ef3 = frame ### Frenet frame (E1,E2,E3)
+                Ni = N[iv_sub] ### SURF-NORMAL
+                "if asym: binormal==Ni; elif geo: binormal == t x Ni"
+                #E3i = Ni if is_asym_or_geo else np.cross(Ti,Ni)
+                if is_asym_or_geo:
+                    "asymptotic; orient binormal with surf-normal changed at inflections"
+                    E3i = Ni
+                    #i = np.where(np.einsum('ij,ij->i',Ef3,E3i)<0)[0]
+                    #E3i[i] = -E3i[i]
+                else:
+                    "geodesic"
+                    E3i = np.cross(Ti,Ni)
+
+                if kck !=1:
+                    "checker_vertex_partial_of_submesh case"
+                    Pi = Pi[::kck]
+                    Ti = Ti[::kck]
+                    E3i = E3i[::kck]
+                    iv_ck = iv_sub[::kck]
+                    seg_vl.extend(iv_ck[:-1])
+                    seg_vr.extend(iv_ck[1:])
+                else:
+                    iv_ck = iv[1:-1]
+                    seg_vl.extend(iv[1:-2])
+                    seg_vr.extend(iv[2:-1])
+
+                bs = BezierSpline(degree=len(Pi)-1,continuity=3,
+                                  efair=efair,itera=200,
+                                  endpoints=Pi,tangents=Ti,normals=E3i)
+
+                if is_Frame:
+                    p, pl, pr = bs.control_points(is_points=True,is_seg=True)
+                    P,Pl,Pr = np.vstack((P,p)), np.vstack((Pl,pl)), np.vstack((Pr,pr))
+                    ctrlP.append(p)
+                    crvp,crvpl,crvpr = bs.control_points(is_curve=True,is_seg=True)
+                    crvPl, crvPr= np.vstack((crvPl,crvpl)), np.vstack((crvPr,crvpr))
+                    
+                    an = np.vstack((an,Pi))
+                    oNi = np.cross(E3i,Ti)
+                    frm1 = np.vstack((frm1,Ti))  ## Frenet-E1
+                    frm2 = np.vstack((frm2,oNi)) ## Frenet-E2
+                    frm3 = np.vstack((frm3,E3i)) ## Frenet-E3
+                    #ruling:
+                    kg,kn,k,tau,d = bs.get_curvature(is_asym_or_geo)
+                    
+                    row_list.append(len(d))
+                    
+                    if False:
+                        i = np.where(np.einsum('ij,ij->i',E3i,d)<0)[0]
+                        d[i] = -d[i]
+
+                    ruling = np.vstack((ruling,d))
+                    all_kg,all_kn = np.r_[all_kg,kg],np.r_[all_kn,kn]
+                    all_k,all_tau = np.r_[all_k,k],np.r_[all_tau,tau]
+                    arr = np.r_[arr, np.arange(len(iv_ck)-1) + num]
+                    num += len(iv_ck)
+                    varr = np.r_[varr,iv_ck]
+                    
+                elif is_ruling and is_dense:
+                    kg,kn,k,tau,pts,d,frmi = bs.get_curvature(is_asym_or_geo,
+                                                            True,num_divide,
+                                                            is_onlyinner,
+                                                            is_modify,
+                                                            is_smooth)
+                    an = np.vstack((an,pts))
+                    frm1 = np.vstack((frm1,frmi[0])) ## Frenet-E1
+                    frm2 = np.vstack((frm2,frmi[1])) ## Frenet-E2
+                    frm3 = np.vstack((frm3,frmi[2])) ## Frenet-E3
+                    
+                    # if False:
+                    #     i = np.where(np.einsum('ij,ij->i',frmi[2],d)<0)[0]
+                    #     d[i] = -d[i]
+                    # elif False:
+                    #     d = -d
+                    
+                    if is_smooth:
+                        from huilab.huimesh.smooth import fair_vertices_or_vectors
+                        Pup = fair_vertices_or_vectors(pts+d,itera=10,efair=is_smooth)
+                        d = Pup-pts
+                        d = d / np.linalg.norm(d,axis=1)[:,None]
+                    
+                    ruling = np.vstack((ruling,d))
+                    all_kg,all_kn = np.r_[all_kg,kg],np.r_[all_kn,kn]
+                    all_k,all_tau = np.r_[all_k,k],np.r_[all_tau,tau]
+                    arr = np.r_[arr, np.arange(len(kg)-1) + num]
+                    num += len(kg)
+                    
+                    row_list.append(len(d))
+                else:
+                    seg_q1234 = np.vstack((seg_q1234,bs.control_points(is_Q1234=True)))
+                    
+            num_poly +=1
+        
+        if is_ruling and is_dense:
+             return [varr,an[1:],ruling[1:],arr,row_list],\
+                 [an[1:],frm1[1:],frm2[1:],frm3[1:]],\
+                     [all_kg,all_kn,all_k,all_tau]
+                    
+        if is_Frame:
+            P, Pl, Pr, crvPl, crvPr = P[1:],Pl[1:],Pr[1:],crvPl[1:],crvPr[1:]   
+            polygon = self.make_polyline_from_endpoints(Pl, Pr)
+            crv = self.make_polyline_from_endpoints(crvPl, crvPr)
+            return P,polygon,crv,np.array(ctrlP,dtype=object),\
+                   [an[1:],frm1[1:],frm2[1:],frm3[1:]],\
+                   [varr,an[1:],ruling[1:],arr,row_list],\
+                   [all_kg,all_kn,all_k,all_tau]
+        else:
+            return seg_q1234[1:],[seg_vl,seg_vr]
